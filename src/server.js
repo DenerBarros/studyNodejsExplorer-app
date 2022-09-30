1433ef685d389baa9ca9099e8bc5897e5633d1dc
DenerBarros <denerbarros.eng@gmail.com>
2022-09-30 13:50:18 -0300

    Criado o scopo do projeto e definindo a arquitetura de separação de responsabilidades da aplicação

---
+++ src/server.js
@@ -0,0 +1,16 @@
+const express = require('express');
+
+const routes = require('./routes')
+
+const app = express();
+app.use(express.json());
+
+app.use(routes);
+
+
+
+const PORT = 3333;
+
+
+
+app.listen(PORT);
