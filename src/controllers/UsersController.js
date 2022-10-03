d26eb2d9e8629842f37639a0440b803dd9ee43de
DenerBarros <denerbarros.eng@gmail.com>
2022-10-03 14:24:51 -0300

    Criado o migrations o sqlite e definido a regra de negócio para criação do users

--- src/controllers/UsersController.js
+++ src/controllers/UsersController.js
@@ -1,10 +1,23 @@
-class UsersController {
+const { hash } = require("bcryptjs");
+const AppError = require("../utils/AppError");
+const sqliteConnection = require("../database/sqlite");
 
-    create(request, response){
-        const { name, email, password } = request.body;
- 
-        response.json({ name, email, password });
-    };
-}
+class UsersController {
+async create(request, response) {
+  const { name, email, password } = request.body;
+  const database = await sqliteConnection();
+  const checkUserExists = await database.get("SELECT * FROM users WHERE email = (?)", [email]);
+
+  if(checkUserExists) {
+    throw new AppError('E-mail já está em uso!');
+  };
+
+  const hashedPassword = await hash(password, 8);
+
+  await database.run("INSERT INTO users (name, email, password) VALUES (?, ?, ?)", [name, email, hashedPassword]);
+
+  return response.status(201).json();
+};
+};
 
 module.exports = UsersController;
