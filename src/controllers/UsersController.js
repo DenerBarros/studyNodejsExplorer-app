38524d0dd7f3e22c335afabf3a02be8495e5d8d1
DenerBarros <denerbarros.eng@gmail.com>
2022-10-03 15:41:42 -0300

    Criado a função de atualizar o cadastro de usuário

--- src/controllers/UsersController.js
+++ src/controllers/UsersController.js
@@ -1,4 +1,4 @@
-const { hash } = require("bcryptjs");
+const { hash, compare } = require("bcryptjs");
 const AppError = require("../utils/AppError");
 const sqliteConnection = require("../database/sqlite");
 
@@ -21,7 +21,7 @@ class UsersController {
     };
 
     async update(request, response) {
-        const {name, email} = request.body;
+        const {name, email, password, old_password} = request.body;
         const { id } = request. params;
 
         const database = await sqliteConnection();
@@ -40,13 +40,29 @@ class UsersController {
         user.name = name;
         user.email = email;
 
+        if(password && !old_password) {
+            throw new AppError("Você precisa informar a senha antiga para definir a nova senha");
+        }
+
+        if(password && old_password) {
+            const checkOldPassword = await compare(old_password, user.password);
+            
+            if(!checkOldPassword) {
+                throw new AppError("Senha antiga não está correta");
+            }
+
+            user.password = await hash(password, 8);
+        }
+
+
         await database.run(`
             UPDATE users SET 
             name = ?,
             email = ?,
-            updated_at = ? 
+            password = ?,
+            updated_at = DATETIME('now')
             WHERE id = ?`,
-            [user.name, user.email, new Date(), id]
+            [user.name, user.email, user.password, id]
         );
 
         return response.status(200).json();
